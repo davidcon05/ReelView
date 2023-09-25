a813201207730a8f5ef5654876d67e516d97e8b2
David Contreras <david.b.contreras@gmail.com>
2023-09-25 19:38:13 -0400

    layout centered top app bar in the scaffold

--- app/src/main/java/com/davecon/reelview/MainActivity.kt
+++ app/src/main/java/com/davecon/reelview/MainActivity.kt
@@ -4,11 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.davecon.reelview.ui.theme.ReelViewTheme
 
@@ -16,31 +25,72 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ReelViewTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            ReelViewApp {
+                MainContent()
             }
         }
     }
 }
 
+/**
+ * Container function great for testing and previewing
+ * Also serves as an entry point for the app, useful
+ * for DI and other things.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ReelViewApp(content: @Composable () -> Unit) {
+    ReelViewTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(
+                            text = "ReelView App",
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier,
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.Menu,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(
+                                imageVector = Icons.Filled.Favorite,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    }
+                )
+            }
+        ) { innerPadding ->
+            val padding = innerPadding
+            MainContent()
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Surface(modifier = Modifier, color = MaterialTheme.colorScheme.background) {
+        Text("Hello Reel View app!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ReelViewPreview() {
     ReelViewTheme {
-        Greeting("Android")
+        ReelViewApp {
+            MainContent()
+        }
     }
 }
