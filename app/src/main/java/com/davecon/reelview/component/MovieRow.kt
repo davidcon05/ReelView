51061f926c17f971f8722c9612f39d4a86ffefa2
David Contreras <david.b.contreras@gmail.com>
2023-09-30 14:18:02 -0400

    implement details screen

--- app/src/main/java/com/davecon/reelview/component/MovieRow.kt
+++ app/src/main/java/com/davecon/reelview/component/MovieRow.kt
@@ -1,8 +1,9 @@
 package com.davecon.reelview.component
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,7 +12,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,13 +22,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
+import coil.size.Scale
 import coil.transform.CircleCropTransformation
 import com.davecon.reelview.model.Movie
 import com.davecon.reelview.model.getMovies
@@ -35,12 +44,13 @@ import com.davecon.reelview.model.getMovies
 @OptIn(ExperimentalMaterial3Api::class, coil.annotation.ExperimentalCoilApi::class)
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0]/*Movie*/, onItemClick: (String) -> Unit = {}) {
+    var expanded by remember { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
         colors = CardDefaults.cardColors(
-            contentColor = Color.Black,
+            contentColor = Black,
             containerColor = Color.White,
         ),
         elevation = CardDefaults.cardElevation(
@@ -58,31 +68,36 @@ fun MovieRow(movie: Movie = getMovies()[0]/*Movie*/, onItemClick: (String) -> Un
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .border(1.dp, Color.Black, RoundedCornerShape(corner = CornerSize(12.dp))),
+                .border(1.dp, Black, RoundedCornerShape(corner = CornerSize(12.dp))),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
         ) {
             Surface(
                 modifier = Modifier
-                    .padding(16.dp)
-                    .size(100.dp),
+                    .padding(8.dp)
+                    .size(80.dp),
                 shape = RectangleShape,
                 color = Color.White
             ) {
                 Image(
                     modifier = Modifier,
-                    painter = rememberImagePainter(data = movie.poster, builder = {
-                        crossfade(true)
-                    }),
+                    painter = rememberImagePainter(data = movie.poster,
+                        builder = {
+                            crossfade(true)
+                            scale(Scale.FIT)
+                        }),
                     contentDescription = "Movie Thumbnail",
                 )
             }
 
-            Column(modifier = Modifier.padding(4.dp)) {
+            Column(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .fillMaxWidth()
+            ) {
                 Text(
                     modifier = Modifier,
                     text = movie.title,
-                    style = MaterialTheme.typography.headlineSmall
+                    style = MaterialTheme.typography.labelLarge
                 )
 
                 Text(
@@ -96,6 +111,52 @@ fun MovieRow(movie: Movie = getMovies()[0]/*Movie*/, onItemClick: (String) -> Un
                     text = "Metascore: ${movie.metascore}",
                     style = MaterialTheme.typography.bodyMedium
                 )
+
+                if (!expanded) {
+                    Icon(
+                        modifier = Modifier
+                            .align(Alignment.End)
+                            .size(24.dp)
+                            .clickable(
+                                onClick = {
+                                    expanded = !expanded
+                                }
+                            ),
+                        tint = Color.DarkGray,
+                        imageVector = Icons.Filled.KeyboardArrowDown,
+                        contentDescription = "Expand"
+                    )
+                } else {
+                    Icon(
+                        modifier = Modifier
+                            .align(Alignment.End)
+                            .size(24.dp)
+                            .clickable(
+                                onClick = {
+                                    expanded = !expanded
+                                }
+                            ),
+                        tint = Color.DarkGray,
+                        imageVector = Icons.Filled.KeyboardArrowUp,
+                        contentDescription = "Collapse"
+                    )
+                }
+
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(
+                            modifier = Modifier,
+                            text = "Plot:",
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            modifier = Modifier,
+                            text = movie.plot,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/davecon/reelview/screen/DetailsScreen.kt
+++ app/src/main/java/com/davecon/reelview/screen/DetailsScreen.kt
@@ -4,9 +4,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -15,42 +22,132 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import coil.compose.ImagePainter.State.Empty.painter
 import coil.compose.rememberImagePainter
+import coil.size.Scale
 import com.davecon.reelview.component.reelViewAppBar
 import com.davecon.reelview.model.Movie
+import com.davecon.reelview.model.getMovies
 
 @OptIn(ExperimentalMaterial3Api::class, coil.annotation.ExperimentalCoilApi::class)
-    @Composable
-    fun DetailsScreen(navController: NavHostController, movie: String?) {
-        Scaffold(
-            topBar = {
-                reelViewAppBar(movie, navController) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = "Back",
-                        modifier = Modifier.clickable {
-                            navController.popBackStack()
-                        }
-                    )
-                }
-            }) { _ ->
-
-        }
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-//            Image(
-//                painter = rememberImagePainter(data = movie),
-//                contentDescription = "Movie Poster Image",
-//                modifier = Modifier.fillMaxSize(),
-//                contentScale = ContentScale.Crop
-//            )
+@Composable
+fun DetailsScreen(navController: NavHostController, movieTitle: String?) {
+    val movie = getMovies().filter { it.title == movieTitle }.first()
+    Scaffold(
+        topBar = {
+            reelViewAppBar(movie.title, navController) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Back",
+                    modifier = Modifier.clickable {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }) { _ ->
+        LazyColumn {
+            item {
+                DetailContent(movie = movie)
+            }
         }
     }
+}
+
+
+// Runtime
+// Genre
+
+// Plot
+
+@Composable
+fun DetailContent(movie: Movie) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 64.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(560.dp)
+                .padding(16.dp),
+            painter = rememberImagePainter(data = movie.poster,
+                builder = {
+                    crossfade(true)
+                    scale(Scale.FILL)
+                }),
+            contentDescription = "Movie Poster",
+        )
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            colors = CardDefaults.cardColors(
+                contentColor = Color.Black,
+                containerColor = Color.White,
+            )
+        ) {
+
+            Text(
+                modifier = Modifier,
+                text = movie.title,
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            Text(
+                modifier = Modifier,
+                text = movie.director.plus(" (${movie.year})"),
+                style = MaterialTheme.typography.labelLarge
+            )
+
+            Text(
+                modifier = Modifier,
+                text = "Rated: ${movie.rated} Metascore: ${movie.metascore}",
+                style = MaterialTheme.typography.labelLarge
+            )
+
+            Text(
+                modifier = Modifier,
+                text = "Actors: ${movie.actors}",
+                style = MaterialTheme.typography.labelLarge
+            )
+
+            Text(
+                modifier = Modifier,
+                text = "Genre: ${movie.genre}",
+                style = MaterialTheme.typography.labelLarge
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    modifier = Modifier,
+                    text = "Plot:",
+                    style = MaterialTheme.typography.labelLarge
+                )
+
+                Text(
+                    modifier = Modifier,
+                    text = "Runtime: ${movie.runtime}",
+                    style = MaterialTheme.typography.labelLarge
+                )
+            }
+
+            Text(
+                modifier = Modifier,
+                text = movie.plot,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+
+    }
+}
